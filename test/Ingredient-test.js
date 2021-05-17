907aeea3809366929331b2cee5093f373732b792
Jim Charnesky <mijnesky@hotmail.com>
2021-05-17 15:21:57 -0600

    Complete steup for test

---
+++ test/Ingredient-test.js
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import Ingredient from '../src/classes/Ingredient';
+
+
+describe('Ingredient', () => {
+  let ingredient;
+  beforeEach('Setup', () => {
+    const newIngredient = {
+      "id": 20081,
+      "name": "wheat flour",
+      "estimatedCostInCents": 142
+    };
+
+    ingredient = new Ingredient(newIngredient);
+  });
+  
+});
