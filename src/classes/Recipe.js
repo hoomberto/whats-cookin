8fb54f339c3cd26834f51cce6e408f2a81523593
Jim Charnesky <mijnesky@hotmail.com>
2021-05-17 16:28:13 -0600

    Start method to return infredientsNeeded

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,3 +1,7 @@
+import ingredients from './ingredients';
+const ingData = ingredients.ingredientsData;
+import Ingredient
+
 class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
@@ -7,6 +11,17 @@ class Recipe {
     this.name = recipe.name;
     this.tags = recipe.tags;
   }
+
+  ingredientsNeeded() {
+        const newIngredients = ingData.map(item => item)
+  // use map to create a new array with ingredient objects
+  // use map to return a new ingredient object
+  // go through each obj in this.ingredients
+
+  // filter ingredients list to
+  // return the ingredient name that matches the id in this.ingredients
+}
+
 }
 export default Recipe;
 // module.exports = Recipe;
