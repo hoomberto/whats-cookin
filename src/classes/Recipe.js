3ddad14e7315cc11617087839409aeca8bf75113
Bobby Vasquez <shootbv@gmail.com>
2021-05-17 14:35:33 -0600

    Add test to check if Recipe contains list of ingredients, add code to pass test

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -2,7 +2,7 @@ class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
     this.image = recipe.image;
-    // this.ingredients = recipe.ingredients;
+    this.ingredients = recipe.ingredients;
     // this.instructions = recipe.instructions;
     // this.name = recipe.name;
     // this.tags = recipe.tags;
