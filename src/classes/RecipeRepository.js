1f80a37c0b05d45fcadf2f442784712b11fba59e
Bobby Vasquez <shootbv@gmail.com>
2021-05-19 16:51:07 -0600

    Remove commented out code and console logs

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -3,28 +3,13 @@ const importedRecipes = recipeData;
 import Recipe from '../classes/Recipe.js';
 
 class RecipeRepository {
-  constructor(recipe) {
-    // this.newRecipe = recipe || undefined;
-    // console.log(recipe);
-    this.recipes = recipe;
-    // this.recipes = recipe || importedRecipes.map(recipe => {
-    //   return new Recipe(recipe);
-    // });
+  constructor(recipes) {
+    this.recipes = recipes;
   }
-  // For every recipe in this.recipes
-  // Check if recipe contains what is passed as an input
-  // for filterByTags method
-  filterByTags(tags) {
-    // const keys = Object.values(this.recipes);
-    // if input is single
-    // let test = this.recipes.filter(recipe => recipe.tags.includes(tags));
-    // if input is an array
-     // console.log('this.recipe tags', this.recipes[0].tags);
-    let test = this.recipes.filter(recipe => recipe.tags.some(tag => tags.includes(tag)));
-    // return arr1.some(item => arr2.includes(item))
-    // let test = this.recipes.filter(recipe => Object.keys());
-    console.log(test)
-    return test
+
+  filterByTags(searchedTags) {
+    let test = this.recipes.filter(recipe => recipe.tags.some(tag => searchedTags.includes(tag)));
+    return test;
   }
 
   filterByProperty() {
