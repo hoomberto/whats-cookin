50c34494ee3cc06e6fb0080a44f5b8c1f528a3c2
Bobby Vasquez <shootbv@gmail.com>
2021-05-20 14:02:22 -0600

    Add test elements to HTML and test code and styling to match

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,27 @@
 import './styles.css';
-import apiCalls from './apiCalls';
-import RecipeRepository from '../classes/RecipeRepository';
-import User from '../src/classes/User';
-import Ingredient from '../src/classes/Ingredient';
-import Recipe from '../src/classes/Recipe';
-import { recipeData } from '../data/recipes';
+// import apiCalls from './apiCalls';
+import RecipeRepository from './classes/RecipeRepository.js';
+import User from './classes/User.js';
+import Ingredient from './classes/Ingredient.js';
+import Recipe from './classes/Recipe.js';
+import { recipeData } from './data/recipes.js';
 const importedRecipes = recipeData;
 
+const getByTag = document.getElementById('getByTag');
+const options = document.getElementById('options')
 
+// getByTag.onClick = expandOptions();
+
+
+
+const expandOptions = () => {
+  options.classList.toggle('hidden')
+  console.log("WORKING")
+}
+
+
+
+// const setTags
 
 // onclick
 const setSiteWideRepository = () => {
@@ -16,3 +30,7 @@ const setSiteWideRepository = () => {
   }))
 }
 console.log('Hello world');
+
+// Event Listeners GO HERE
+
+getByTag.addEventListener("click", expandOptions);
