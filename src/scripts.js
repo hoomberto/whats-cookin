2c437cdc8f709d8d635cd38116e6f3d9b5e3f9ec
Bobby Vasquez <shootbv@gmail.com>
2021-05-20 13:26:26 -0600

    Add imports to scripts.js, link scripts.js to index.html file, create setRepository function

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,18 @@
 import './styles.css';
 import apiCalls from './apiCalls';
+import RecipeRepository from '../classes/RecipeRepository';
+import User from '../src/classes/User';
+import Ingredient from '../src/classes/Ingredient';
+import Recipe from '../src/classes/Recipe';
+import { recipeData } from '../data/recipes';
+const importedRecipes = recipeData;
 
 
 
-
+// onclick
+const setSiteWideRepository = () => {
+  return new RecipeRepository(importedRecipes.map(recipe => {
+    return new Recipe(recipe)
+  }))
+}
 console.log('Hello world');
